d05bbee5763f1adca2de48fe6db84f97c5568a8a
Mohan Rathod <mohan.rathod@robosoftin.com>
2020-06-13 20:55:00 +0530

    Added a get by gender function

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
+
+var R = require('ramda');
 var ura = require('unique-random-array');
 var starwarObject = require('./strwars-object.json');
+var male = "male";
+var female = "female";
+
 
 module.exports = {
     all: starwarObject,
     random: ura(starwarObject),
+    getMale: getByGender(male),
+    getFemale: getByGender(female)
+}
 
+function getByGender(givenGender){
+    const getByGender = R.filter(R.propEq('gender', givenGender))(starwarObject);
+    return getByGender;
 }
